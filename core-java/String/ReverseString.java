98887ce51f8c61c669720db10802000ee7bcb5c5
vinotha-somasundaram <vinotha.somu@gmail.com>
2025-12-28 13:00:08 +0530

    Update ReverseString.java

--- core-java/String/ReverseString.java
+++ core-java/String/ReverseString.java
@@ -1,1 +1,18 @@
+/*Problem Reverse a String
+Input Zoho
+Output ohoZ
+Approach Loop from end to start and append characters*/
+
+public class ReverseString {
+    public static void main(String[] args)
+   {
+        String s = Zoho;
+        String rev = ;
+        for (int i = s.length() - 1; i = 0; i--) 
+       {
+          rev += s.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
 
